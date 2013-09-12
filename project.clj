c1fe0ec82406fad381b2064ca08347544db4efae
Michael Grubb <devel@dailyvoid.com>
2013-09-11 21:22:28 -0500

    updated to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-unidecode "1.0.0"
+(defproject clj-unidecode "1.0.1"
   :description "Clojure implementation of Junidecode"
   :url "http://github.com/mgrubb/clj-unidecode"
   :license {:name "BSD"
             :url "http://www.debian.org/misc/bsd.license"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.6-SNAPSHOT"]]}}
   :min-lein-version "2.0.0"
   :pom-addition [:developers [:developer
                               [:name "Michael Grubb"]
